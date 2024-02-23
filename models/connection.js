3824a6d8424702afae75069873269b7989ffb731
jfbonglet <jean.francois.bonglet@gmail.com>
2024-02-23 16:49:38 +0100

    MAJ setting DB + vercel.json

--- models/connection.js
+++ models/connection.js
@@ -5,7 +5,7 @@ const connectionString = process.env.CONNEXION_STRING;
 
 //Connection à la base de données qui renvoie un message de confirmation
 mongoose
-  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .connect(connectionString)
   .then(() => console.log("Database connected"))
   .catch(error => console.error(error));
 
